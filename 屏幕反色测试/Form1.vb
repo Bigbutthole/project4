c0a7a870a05a4e25ab38e06f4850b33247b260fe
Bigbutt <313947568@qq.com>
2019-08-07 19:48:50 +0800

    添加项目文件。

---
+++ 屏幕反色测试/Form1.vb
@@ -0,0 +1,12 @@
+﻿Imports 反色模块
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim www As Graphics = Graphics.FromHdc(ApiClass.GetWindowDC(ApiClass.GetDesktopWindow))
+        Dim scr As Screen = My.Computer.Screen
+        Dim temp1 As New Bitmap(scr.WorkingArea.Width, scr.WorkingArea.Height)
+        Dim ww As Graphics = Graphics.FromImage(temp1)
+        ww.CopyFromScreen(New Point, New Point, scr.WorkingArea.Size)
+        Dim handle As IntPtr = Class1.PContray(temp1).GetHicon
+        www.DrawIcon(Icon.FromHandle(handle), scr.WorkingArea)
+    End Sub
+End Class
---
+++ 屏幕反色测试/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' 有关程序集的一般信息由以下
+' 控制。更改这些特性值可修改
+' 与程序集关联的信息。
+
+'查看程序集特性的值
+
+<Assembly: AssemblyTitle("屏幕反色测试")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("Microsoft")>
+<Assembly: AssemblyProduct("屏幕反色测试")>
+<Assembly: AssemblyCopyright("Copyright © Microsoft 2009")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'如果此项目向 COM 公开，则下列 GUID 用于 typelib 的 ID
+<Assembly: Guid("cbe68b5d-6cc7-45b6-8c88-c0c2be693b6a")>
+
+' 程序集的版本信息由下列四个值组成: 
+'
+'      主版本
+'      次版本
+'      生成号
+'      修订号
+'
+'可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值
+'通过使用 "*"，如下所示:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
